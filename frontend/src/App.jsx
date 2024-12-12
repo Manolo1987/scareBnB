6ed238b93e0ab45130e7f394c6dd51e0d61cc886
Andreas <andreas.knopf@dci-student.org>
2024-12-12 15:14:15 +0100

    account dropdown menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,12 @@ import NotFound from './pages/NotFound/NotFound.jsx';
 import Information from './pages/Information/Information.jsx';
 import UserAuthContextProvider from './context/UserAuthContext.jsx';
 import AccommodationContextProvider from './context/AccommodationContext.jsx';
+import Profile from './components/Account/Profile/Profile.jsx';
+import Favourites from './components/Account/Favourites/Favourites.jsx';
+import Bookings from './components/Account/Bookings/Bookings.jsx';
+import Listings from './components/Account/Listings/Listings.jsx';
+import AdminUserList from './components/Account/AdminUserList/AdminUserList.jsx';
+import AdminAccoList from './components/Account/AdminAccoList/AdminAccoList.jsx';
 
 export default function App() {
   return (
@@ -21,7 +27,14 @@ export default function App() {
             <Route index element={<HomePage />} />
             <Route path='accommodation' element={<Accommodation />} />
             <Route path='accommodationlist' element={<AccommodationList />} />
-            <Route path='account' element={<Account />} />
+            <Route path='account' element={<Account />}>
+              <Route path='profile' element={<Profile />} />
+              <Route path='favourites' element={<Favourites />} />
+              <Route path='bookings' element={<Bookings />} />
+              <Route path='listings' element={<Listings />} />
+              <Route path='adminUserList' element={<AdminUserList />} />
+              <Route path='adminAccoList' element={<AdminAccoList />} />
+            </Route>
             <Route path='booking' element={<Booking />} />
             <Route path='information' element={<Information />} />
           </Route>
@@ -29,6 +42,5 @@ export default function App() {
         </Routes>
       </AccommodationContextProvider>
     </UserAuthContextProvider>
-  )
+  );
 }
-
--- frontend/src/pages/Account/Account.jsx
+++ frontend/src/pages/Account/Account.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import styles from './Account.module.css'
+import styles from './Account.module.css';
+import Header from '../../components/Shared/Header/Header.jsx';
+import AccountNav from '../../components/Account/AccountNav/AccountNav.jsx';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../components/Shared/Footer/Footer.jsx';
 
 export default function Account() {
   return (
-    <div>Account</div>
-  )
+    <>
+      <AccountNav />
+      <Outlet />
+    </>
+  );
 }
