6056641d71d1d5ad517e1d20d0e6073f65aca8ee
TamaChi <TamaChi@gmail.com>
2024-12-17 11:49:03 +0100

    building homepage & adjusting styles

--- frontend/src/components/Shared/Header/Header.jsx
+++ frontend/src/components/Shared/Header/Header.jsx
@@ -4,6 +4,7 @@ import Login from '../Login/Login';
 import Register from '../Register/Register';
 import AccountDropdownMenu from '../../Account/AccountDropdownMenu/AccountDropdownMenu';
 import { useAuth } from '../../../context/UserAuthContext';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   const [showLogin, setShowLogin] = useState(false);
@@ -19,10 +20,12 @@ export default function Header() {
     <header>
       <section className={styles.top_banner}>
         <div className={styles.icon_container}>
-          <img
-            src='https://st4.depositphotos.com/14009552/23169/i/450/depositphotos_231693396-stock-photo-paint-home-vector-logo.jpg'
-            alt='website-icon'
-          />
+          <Link to='/'>
+            <img
+              src='https://st4.depositphotos.com/14009552/23169/i/450/depositphotos_231693396-stock-photo-paint-home-vector-logo.jpg'
+              alt='website-icon'
+            />
+          </Link>
         </div>
         <div className={styles.account}>
           {isAuthenticated ? (
