16e40c3debaeae615a2f85cae7c87deea26000b5
Andreas <andreas.knopf@dci-student.org>
2024-12-17 16:32:23 +0100

    Update2 Profile

--- frontend/src/components/Account/Profile/Profile.jsx
+++ frontend/src/components/Account/Profile/Profile.jsx
@@ -5,6 +5,8 @@ import { useAuth } from '../../../context/UserAuthContext.jsx';
 export default function Profile({ showPassword, togglePasswordVisibility }) {
   const { user } = useAuth();
 
+  console.log(user);
+
   const [isEditing, setIsEditing] = useState(false);
   const [formData, setFormData] = useState({
     firstName: '',
@@ -72,188 +74,198 @@ export default function Profile({ showPassword, togglePasswordVisibility }) {
   };
 
   return (
-    <div className={styles.myProfile}>
-      <h2>Mein Profil</h2>
-      <div className={styles.profileInfo}>
-        <table>
-          <tr>
-            <td>
-              <span>Name: </span>
-            </td>
-            <td>
-              {user?.firstName} {user?.lastName}
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span>Email: </span>
-            </td>
-            <td>{user?.email}</td>
-          </tr>
-          <tr>
-            <td>
-              <span>Phone: </span>
-            </td>
-            <td>{user?.phone}</td>
-          </tr>
-          <tr>
-            <td>
-              <span>Date of Birth: </span>
-            </td>
-            <td>{new Date(user?.dateOfBirth).toLocaleDateString('de-DE')}</td>
-          </tr>
-        </table>
-      </div>
+    <div className={styles.profileContainer}>
+      <div className={styles.myProfile}>
+        <h2>Mein Profil</h2>
+        <div className={styles.profileInfo}>
+          <table>
+            <tr>
+              <td>
+                <span>Name: </span>
+              </td>
+              <td>
+                {user?.firstName} {user?.lastName}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span>Email: </span>
+              </td>
+              <td>{user?.email}</td>
+            </tr>
+            <tr>
+              <td>
+                <span>Phone: </span>
+              </td>
+              <td>{user?.phone}</td>
+            </tr>
+            <tr>
+              <td>
+                <span>Date of Birth: </span>
+              </td>
+              <td>{new Date(user?.dateOfBirth).toLocaleDateString('de-DE')}</td>
+            </tr>
+          </table>
+        </div>
 
-      <div className={styles.editProfile}>
-        {isEditing && (
-          <div className={styles.editForm}>
-            <form onSubmit={handleSubmit}>
-              <label>
-                First Name:
-                <input
-                  type='text'
-                  name='firstName'
-                  value={user?.firstName}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.firstName && (
-                  <p className={styles.error}>{errors.firstName}</p>
-                )}
-              </label>
+        <div className={styles.editProfile}>
+          {isEditing && (
+            <div className={styles.editForm}>
+              <form onSubmit={handleSubmit}>
+                <label>
+                  First Name:
+                  <input
+                    type='text'
+                    name='firstName'
+                    value={user?.firstName}
+                    onChange={handleChange}
+                    required
+                  />
+                  {errors.firstName && (
+                    <p className={styles.error}>{errors.firstName}</p>
+                  )}
+                </label>
 
-              <label>
-                Last Name:
-                <input
-                  type='text'
-                  name='lastName'
-                  value={user?.lastName}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.lastName && (
-                  <p className={styles.error}>{errors.lastName}</p>
-                )}
-              </label>
+                <label>
+                  Last Name:
+                  <input
+                    type='text'
+                    name='lastName'
+                    value={user?.lastName}
+                    onChange={handleChange}
+                    required
+                  />
+                  {errors.lastName && (
+                    <p className={styles.error}>{errors.lastName}</p>
+                  )}
+                </label>
 
-              <label>
-                Email:
-                <input
-                  type='email'
-                  name='email'
-                  value={user?.email}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.email && <p className={styles.error}>{errors.email}</p>}
-              </label>
+                <label>
+                  Email:
+                  <input
+                    type='email'
+                    name='email'
+                    value={user?.email}
+                    onChange={handleChange}
+                    required
+                  />
+                  {errors.email && (
+                    <p className={styles.error}>{errors.email}</p>
+                  )}
+                </label>
 
-              <label>
-                Password:
-                <input
-                  type={showPassword ? 'text' : 'password'}
-                  name='password'
-                  value={user?.password}
-                  onChange={handleChange}
-                  required
-                />
-                <span
-                  className={styles.icon}
-                  onClick={togglePasswordVisibility}
-                  role='button'
-                  aria-label={showPassword ? 'Hide password' : 'Show password'}
-                >
-                  {showPassword ? '👻' : '👁️'}
-                </span>
-                {errors.password && (
-                  <p className={styles.error}>{errors.password}</p>
-                )}
-              </label>
+                <label>
+                  Password:
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    name='password'
+                    value={user?.password}
+                    onChange={handleChange}
+                    required
+                  />
+                  <span
+                    className={styles.icon}
+                    onClick={togglePasswordVisibility}
+                    role='button'
+                    aria-label={
+                      showPassword ? 'Hide password' : 'Show password'
+                    }
+                  >
+                    {showPassword ? '👻' : '👁️'}
+                  </span>
+                  {errors.password && (
+                    <p className={styles.error}>{errors.password}</p>
+                  )}
+                </label>
 
-              <label>
-                Confirm Password:
-                <input
-                  type={showConfirmPassword ? 'text' : 'password'}
-                  name='confirmPassword'
-                  value={user?.confirmPassword}
-                  onChange={handleChange}
-                  required
-                />
-                <span
-                  className={styles.icon}
-                  onClick={toggleConfirmPasswordVisibility}
-                  role='button'
-                  aria-label={
-                    showConfirmPassword ? 'Hide password' : 'Show password'
-                  }
-                >
-                  {showConfirmPassword ? '👻' : '👁️'}
-                </span>
-                {errors.confirmPassword && (
-                  <p className={styles.error}>{errors.confirmPassword}</p>
-                )}
-              </label>
+                <label>
+                  Confirm Password:
+                  <input
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    name='confirmPassword'
+                    value={user?.confirmPassword}
+                    onChange={handleChange}
+                    required
+                  />
+                  <span
+                    className={styles.icon}
+                    onClick={toggleConfirmPasswordVisibility}
+                    role='button'
+                    aria-label={
+                      showConfirmPassword ? 'Hide password' : 'Show password'
+                    }
+                  >
+                    {showConfirmPassword ? '👻' : '👁️'}
+                  </span>
+                  {errors.confirmPassword && (
+                    <p className={styles.error}>{errors.confirmPassword}</p>
+                  )}
+                </label>
 
-              <label>
-                Phone:
-                <input
-                  type='tel'
-                  name='phone'
-                  value={user?.phone}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.phone && <p className={styles.error}>{errors.phone}</p>}
-              </label>
+                <label>
+                  Phone:
+                  <input
+                    type='tel'
+                    name='phone'
+                    value={user?.phone}
+                    onChange={handleChange}
+                    required
+                  />
+                  {errors.phone && (
+                    <p className={styles.error}>{errors.phone}</p>
+                  )}
+                </label>
 
-              <label>
-                Birthday:
-                <input
-                  type='date'
-                  name='dateOfBirth'
-                  value={user?.dateOfBirth}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.dateOfBirth && (
-                  <p className={styles.error}>{errors.dateOfBirth}</p>
-                )}
-              </label>
+                <label>
+                  Birthday:
+                  <input
+                    type='text'
+                    name='dateOfBirth'
+                    value={new Date(user?.dateOfBirth).toLocaleDateString(
+                      'de-DE'
+                    )}
+                    onChange={handleChange}
+                    required
+                  />
+                  {errors.dateOfBirth && (
+                    <p className={styles.error}>{errors.dateOfBirth}</p>
+                  )}
+                </label>
 
-              <label className={styles.checkbox}>
-                <input
-                  type='checkbox'
-                  name='dataConsent'
-                  checked={user?.dataConsent}
-                  onChange={handleChange}
-                  required
-                />
-                I agree to the terms and conditions
-              </label>
-            </form>
-          </div>
+                {/* <label className={styles.checkbox}>
+                  <input
+                    type='checkbox'
+                    name='dataConsent'
+                    checked={user?.dataConsent}
+                    onChange={handleChange}
+                    required
+                  />
+                  I agree to the terms and conditions
+                </label> */}
+              </form>
+            </div>
+          )}
+        </div>
+
+        {!isEditing ? (
+          <button
+            className={styles.editProfileButton}
+            onClick={() => setIsEditing(true)}
+          >
+            edit profile
+          </button>
+        ) : (
+          <>
+            <button className={styles.saveButton}>save</button>
+            <button
+              className={styles.cancelButton}
+              onClick={() => setIsEditing(false)}
+            >
+              cancel
+            </button>
+          </>
         )}
       </div>
-
-      {!isEditing ? (
-        <button
-          className={styles.editProfileButton}
-          onClick={() => setIsEditing(true)}
-        >
-          edit profile
-        </button>
-      ) : (
-        <>
-          <button className={styles.saveButton}>save</button>
-          <button
-            className={styles.cancelButton}
-            onClick={() => setIsEditing(false)}
-          >
-            cancel
-          </button>
-        </>
-      )}
     </div>
   );
 }
