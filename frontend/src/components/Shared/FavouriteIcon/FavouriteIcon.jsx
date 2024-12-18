7cf90f993f32391eb265a95248702e2d4cb38bab
Danny Köhler-Poetsch <danny_koehler-poetsch@gmx.net>
2024-12-18 15:47:29 +0100

    add FavouriteIcon to AccoCard without testing

--- frontend/src/components/Shared/FavouriteIcon/FavouriteIcon.jsx
+++ frontend/src/components/Shared/FavouriteIcon/FavouriteIcon.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
-import styles from './FavoutiteIcon.module.css'
+import React, { useState } from 'react';
+import styles from './FavouriteIcon.module.css';
+import { HeartStraight } from '@phosphor-icons/react';
+import { useAuth } from '../../../context/UserAuthContext';
 
-export default function FavouriteIcon() {
+export default function FavouriteIcon({ accoId }) {
+  const [isFavourite, setIsFavourite] = useState(false);
+  const { addFavourite, removeFavourite } = useAuth();
+
+  const handleAddFavourite = (e) => {
+    e.stopPropagation();
+    setIsFavourite(true);
+    addFavourite(accoId);
+  };
+
+  const handleRemoveFavourite = (e) => {
+    e.stopPropagation();
+    setIsFavourite(false);
+    removeFavourite(accoId);
+  };
   return (
-    <div>FavouriteIcon</div>
-  )
+    <>
+      {!isFavourite ? (
+        <div>
+          <HeartStraight
+            size={32}
+            color='#f70202'
+            onClick={handleAddFavourite}
+          />
+        </div>
+      ) : (
+        <div>
+          <HeartStraight
+            size={32}
+            color='#f70202'
+            weight='fill'
+            onClick={handleRemoveFavourite}
+          />
+        </div>
+      )}
+    </>
+  );
 }
