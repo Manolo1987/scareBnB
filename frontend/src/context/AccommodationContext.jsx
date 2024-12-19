a02d3d6f37d307b496a475c6d52842816c7ae47a
TamaChi <TamaChi@gmail.com>
2024-12-19 14:06:11 +0100

    added sort/filter to useEffect

--- frontend/src/context/AccommodationContext.jsx
+++ frontend/src/context/AccommodationContext.jsx
@@ -22,7 +22,7 @@ export default function AccommodationContextProvider({ children }) {
   //pagination
   const [currentPage, setCurrentPage] = useState(2);
 
-  async function getAllAccommodations(limit = 21, currentPage = 1) {
+  async function getAllAccommodations(limit) {
     // apply loading state here
     try {
       const query = `?state=${stateFilter}&maxPrice=${maxPrice}&minPrice=${minPrice}&bedrooms=${bedrooms}&minRating=${minRating}&sortBy=${sortBy}&sortOrder=${sortOrder}&page=${currentPage}&limit=${limit}`;
