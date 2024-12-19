c9c0212e77c2ac70e03b1fa05d72e5bea99633a1
j-grzy <jana_grzywacz@web.de>
2024-12-19 12:15:45 +0100

    add ScrollRestoration component to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollRestoration from './components/Shared/ScrollRestoration/ScrollRestoration.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <ScrollRestoration />
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
