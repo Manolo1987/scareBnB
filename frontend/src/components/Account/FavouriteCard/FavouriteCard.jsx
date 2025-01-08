642d82e39953c0c22d0f5372d565f62811275a4d
Andreas <andreas.knopf@dci-student.org>
2025-01-08 16:39:39 +0100

    create favoritesComponent

--- frontend/src/components/Account/FavouriteCard/FavouriteCard.jsx
+++ frontend/src/components/Account/FavouriteCard/FavouriteCard.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import styles from './FavouriteCard.module.css'
+import styles from './FavouriteCard.module.css';
+import FavouriteIcon from '../../Shared/FavouriteIcon/FavouriteIcon.jsx';
 
-export default function FavouriteCard() {
+export default function FavouriteCard({ favourite }) {
   return (
-    <div>FavouriteCard</div>
-  )
+    <div className={styles.favouriteCard}>
+      <img
+        src={favourite.titleImage.secure_url}
+        alt={favourite.title}
+        className={styles.image}
+      />
+      <FavouriteIcon />
+      <h3>{favourite.title}</h3>
+      <p>
+        {favourite.city}, {favourite.state}
+      </p>
+      <p>{favourite.description}</p>
+      <p>{favourite.pricePerNight} €/night</p>
+    </div>
+  );
 }
--- frontend/src/components/Account/Favourites/Favourites.jsx
+++ frontend/src/components/Account/Favourites/Favourites.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Favourites.module.css';
-import FavouriteIcon from '../../Shared/FavouriteIcon/FavouriteIcon';
+import FavouriteIcon from '../../Shared/FavouriteIcon/FavouriteIcon.jsx';
+import FavouriteCard from '../FavouriteCard/FavouriteCard.jsx';
+import { useAuth } from '../../../context/UserAuthContext.jsx';
 
 export default function Favourites() {
+  const { favourites } = useAuth();
+  console.log('favourites', favourites);
+
   return (
     <div>
-      <FavouriteIcon />
+      <h1 className={styles.title}>Favourites</h1>
+      {/* <FavouriteIcon /> */}
+      {favourites.length > 0 ? (
+        <div className={styles.favouritesList}>
+          {favourites.map((fav) => (
+            <FavouriteCard key={fav._id} favourite={fav} />
+          ))}
+        </div>
+      ) : (
+        <p>No favourites yet!</p>
+      )}
     </div>
   );
 }
