1a63984fb86710dfdd6c724adf028c02e9a3c8b4
j-grzy <jana_grzywacz@web.de>
2025-01-08 09:33:48 +0100

    Listings component

--- frontend/src/components/Account/Listings/Listings.jsx
+++ frontend/src/components/Account/Listings/Listings.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Listings.module.css';
 import ListingsCard from '../ListingsCard/ListingsCard.jsx';
 import ListingsNav from '../ListingsNav/ListingsNav.jsx';
+import { useAcco } from '../../../context/AccommodationContext.jsx';
+import { Link } from 'react-router-dom';
 
 export default function Listings() {
+  const { myListings, getMyListings } = useAcco();
+  useEffect(() => {
+    getMyListings();
+  }, []);
+
   return (
     <div>
       <ListingsNav />
-      My Listings
-      <ListingsCard />
+      {myListings?.length < 1 && (
+        <p>
+          You don't have any listings yet.{' '}
+          <Link to='/account/handleListings'>
+            Create your first Listing here.
+          </Link>
+        </p>
+      )}
+      {myListings?.length > 0 && (
+        <div className={styles.mylistings_list}>
+          <h1>My Listings</h1>
+          <ul>
+            {myListings?.map((listing, index) => {
+              return (
+                <li key={index}>
+                  <ListingsCard listing={listing} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
