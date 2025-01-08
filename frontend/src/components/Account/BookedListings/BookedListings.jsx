f08c96f81b2a05832ee941d245f2d132ae5f858b
j-grzy <jana_grzywacz@web.de>
2025-01-08 11:40:47 +0100

    add Buttons and content to ListingsCard

--- frontend/src/components/Account/BookedListings/BookedListings.jsx
+++ frontend/src/components/Account/BookedListings/BookedListings.jsx
@@ -4,6 +4,10 @@ import ListingsNav from '../ListingsNav/ListingsNav.jsx';
 import BookedListingsCard from '../BookedListingsCard/BookedListingsCard.jsx';
 
 export default function BookedListings() {
+  const { myListings, getMyBookedListings } = useAcco();
+  useEffect(() => {
+    getMyBookedListings();
+  }, []);
   return (
     <div>
       <ListingsNav />
--- frontend/src/components/Account/ListingsCard/ListingsCard.jsx
+++ frontend/src/components/Account/ListingsCard/ListingsCard.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import styles from './ListingsCard.module.css'
+import styles from './ListingsCard.module.css';
+import { Link } from 'react-router-dom';
+import { Trash, Pencil } from '@phosphor-icons/react';
 
-export default function ListingsCard() {
+export default function ListingsCard({ listing }) {
   return (
-    <div>ListingsCard</div>
-  )
+    <Link
+      to={`/accommodationList/${listing.title
+        .toLowerCase()
+        .replace(/\s+/g, '-')}?id=${listing._id}`}
+      state={{ id: listing._id }}
+      className={styles.listingCardLink}
+    >
+      <div className={styles.listingCard}>
+        {/* add listing infos and styling here */}
+        {listing.title}
+        {listing.city}
+        {listing.rating.toFixed(1)}
+        {listing.bedroms}
+        {listing.pricePerNight}
+
+        <button className={styles.listingsButton}>
+          <Pencil size={32} />
+        </button>
+        <button className={styles.listingsButton}>
+          <Trash size={32} />
+        </button>
+      </div>
+    </Link>
+  );
 }
