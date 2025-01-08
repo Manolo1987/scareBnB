320c5c0fc82ffb5cdaa821e430db98f396745cf9
j-grzy <jana_grzywacz@web.de>
2025-01-08 11:57:55 +0100

    add bookedListingCard content

--- frontend/src/components/Account/BookedListings/BookedListings.jsx
+++ frontend/src/components/Account/BookedListings/BookedListings.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './BookedListings.module.css';
 import ListingsNav from '../ListingsNav/ListingsNav.jsx';
 import BookedListingsCard from '../BookedListingsCard/BookedListingsCard.jsx';
+import { useBooking } from '../../../context/bookingContext.jsx';
 
 export default function BookedListings() {
-  const { myListings, getMyBookedListings } = useAcco();
+  const { myBookedListings, getMyBookedListings } = useBooking();
   useEffect(() => {
     getMyBookedListings();
   }, []);
   return (
     <div>
       <ListingsNav />
-      BookedListings
-      <BookedListingsCard />
+      {myBookedListings?.length < 1 && (
+        <div className={styles.messageContainer}>
+          <p className={styles.message}>
+            You don't have any booked listings at the moment.
+          </p>
+        </div>
+      )}
+      {myBookedListings?.length > 0 && (
+        <div className={styles.myBookedlistings_list}>
+          <h1>My Booked Listings</h1>
+          <ul>
+            {myBookedListings?.map((listing, index) => {
+              return (
+                <li key={index}>
+                  <BookedListingsCard listing={listing} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/Account/BookedListingsCard/BookedListingsCard.jsx
+++ frontend/src/components/Account/BookedListingsCard/BookedListingsCard.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import styles from './BookedListingsCard.module.css';
 
-export default function BookedListingsCard() {
-  return <div>BookedListingsCard</div>;
+export default function BookedListingsCard({ listing }) {
+  return (
+    <div className={styles.bookedListingCard}>
+      {/* add booked listing infos and styling here */}
+      {listing.title}
+      {listing.city}
+      {listing.rating.toFixed(1)}
+      {listing.bedroms}
+      {listing.pricePerNight}
+      {listing.numberOfGuests}
+      {listing.checkIn}
+      {listing.checkOut}
+      {listing.paymentMethod}
+    </div>
+  );
 }
