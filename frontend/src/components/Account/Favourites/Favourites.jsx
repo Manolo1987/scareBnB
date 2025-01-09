713c007dbdca52d7c8375a47050d7c405d5c3c1a
Andreas <andreas.knopf@dci-student.org>
2025-01-09 10:23:02 +0100

    add favouritesComponent

--- frontend/src/components/Account/Favourites/Favourites.jsx
+++ frontend/src/components/Account/Favourites/Favourites.jsx
@@ -1,26 +1,29 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styles from './Favourites.module.css';
-import FavouriteIcon from '../../Shared/FavouriteIcon/FavouriteIcon.jsx';
-import FavouriteCard from '../FavouriteCard/FavouriteCard.jsx';
 import { useAuth } from '../../../context/UserAuthContext.jsx';
+import AccoCard from '../../Shared/AccoCard/AccoCard.jsx';
 
 export default function Favourites() {
   const { favourites } = useAuth();
   console.log('favourites', favourites);
 
   return (
-    <div>
-      <h1 className={styles.title}>Favourites</h1>
-      {/* <FavouriteIcon /> */}
-      {favourites.length > 0 ? (
-        <div className={styles.favouritesList}>
-          {favourites.map((fav) => (
-            <FavouriteCard key={fav._id} favourite={fav} />
-          ))}
-        </div>
-      ) : (
-        <p>No favourites yet!</p>
-      )}
+    <div className={styles.favouritesContainer}>
+      <div className={styles.accoGallery}>
+        <h1 className={styles.title}>Favourites</h1>
+
+        {favourites.length > 0 ? (
+          <div className={styles.accoList}>
+            {favourites.map((fav) => (
+              <div className={styles.accoCard} key={fav._id}>
+                <AccoCard acco={fav} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No favourites yet!</p>
+        )}
+      </div>
     </div>
   );
 }
