a6b3b953abfe908ddee25bc6c042bd5df33fbe86
j-grzy <jana_grzywacz@web.de>
2025-01-15 16:13:59 +0100

    apply global and local styling to BookedListings Component

--- frontend/src/components/Account/BookedListingsCard/BookedListingsCard.jsx
+++ frontend/src/components/Account/BookedListingsCard/BookedListingsCard.jsx
@@ -1,72 +1,67 @@
 import React from 'react';
 import '../../../App.css';
 import styles from './BookedListingsCard.module.css';
+import { Envelope } from '@phosphor-icons/react';
 
 export default function BookedListingsCard({ listing }) {
   //console.log(listing);
   return (
-    <div className='cardContainer'>
+    <div className={`cardContainer ${styles.cardContainer_special}`}>
       {/* hier eventuell globale styles anwenden */}
-      <div className={styles.info_container}>
-        <div className={styles.img_container}>
-          <h3 className={styles.cardTitle}>
-            {listing.accommodation.title} ({listing.accommodation.city})
-          </h3>
-          <img
-            src={listing.accommodation.titleImage.secure_url}
-            alt={listing.accommodation.title}
-            className={styles.cardImage}
-          />
+
+      <div className='imgContainer'>
+        <img
+          src={listing.accommodation.titleImage.secure_url}
+          alt={listing.accommodation.title}
+          className='cardImage'
+        />
+      </div>
+
+      <div className='infoContainer'>
+        <div className='infoHeader'>
+          <span className='cardTitle'>{listing.accommodation.title}</span>
+          <span className='cardCity'>{listing.accommodation.city}</span>
+        </div>
+        <div className='infoBody'>
+          <p>
+            Guest: {listing.guest.firstName} {listing.guest.lastName}
+          </p>
+          <p className={styles.cardNumberOfGuests}>
+            Number of Guests: {listing.numberOfGuests}
+          </p>
+          <p className={styles.cardCheckIn}>
+            Check In: {new Date(listing.checkIn).toLocaleDateString()}
+          </p>
+          <p className={styles.cardCheckOut}>
+            Check Out: {new Date(listing.checkOut).toLocaleDateString()}
+          </p>
+          <p className={styles.cardPaymentMethod}>
+            Payment Method: {listing.paymentMethod}
+          </p>
+          <p className={styles.cardTotalPrice}>
+            Total Price: {listing.totalPrice}€
+          </p>
         </div>
       </div>
-      <div className={styles.info_container}>
-        <p className={styles.cardRating}>
-          Rating: {listing.accommodation.rating.toFixed(1)}
-        </p>
-        <p className={styles.cardBedrooms}>
-          Bedrooms: {listing.accommodation.bedrooms}
-        </p>
-        <p className={styles.cardPricePerNight}>
-          Price per Night: {listing.accommodation.pricePerNight}€
-        </p>
-      </div>
-      <div className={styles.info_container}>
-        <p>
-          Guest: {listing.guest.firstName} {listing.guest.lastName}
-        </p>
-        <p className={styles.cardNumberOfGuests}>
-          Number of Guests: {listing.numberOfGuests}
-        </p>
-        <p className={styles.cardCheckIn}>
-          Check In: {new Date(listing.checkIn).toLocaleDateString()}
-        </p>
-        <p className={styles.cardCheckOut}>
-          Check Out: {new Date(listing.checkOut).toLocaleDateString()}
-        </p>
-        <p className={styles.cardPaymentMethod}>
-          Payment Method: {listing.paymentMethod}
-        </p>
-        <p className={styles.cardTotalPrice}>
-          Total Price: {listing.totalPrice}€
-        </p>
-        {listing.isCancelled && (
-          <div className={styles.info_container}>
-            <span
-              className={styles.cancelledSpan}
-              title='Your booking has been cancelled'
-            >
-              Booking cancelled by guest
-            </span>
-          </div>
-        )}
+      <div className={styles.contactLinkContainer}>
         <a
           href={`mailto:${listing.guest.email}`}
           className={styles.contactLink}
           title='Contact Host'
         >
-          Contact Guest
+          <Envelope size={32} className={styles.icon} />
         </a>
       </div>
+      {listing.isCancelled && (
+        <div className={styles.cancelledContainer}>
+          <span
+            className={styles.cancelledSpan}
+            title='Your booking has been cancelled'
+          >
+            Cancelled
+          </span>
+        </div>
+      )}
     </div>
   );
 }
