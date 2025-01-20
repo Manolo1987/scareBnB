a6759aca2b01c89d7106ca22d2ace3f051c54945
TamaChi <TamaChi@gmail.com>
2025-01-20 15:02:36 +0100

    optimizing display

--- frontend/src/pages/Accommodation/Accommodation.jsx
+++ frontend/src/pages/Accommodation/Accommodation.jsx
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 import styles from './Accommodation.module.css';
 import Search from '../../components/Shared/Search/Search.jsx';
 import { useAcco } from '../../context/AccommodationContext.jsx';
-import AccoSlider from '../../components/Accomodation/AccoSlider/AccoSlider.jsx';
 import AccoInfo from '../../components/Accomodation/AccoInfo/AccoInfo.jsx';
-import AccoMap from '../../components/Accomodation/AccoMap/AccoMap.jsx';
 import BookingPreview from '../../components/Accomodation/BookingPreview/BookingPreview.jsx';
 import { useLocation } from 'react-router-dom';
 import Comments from '../../components/Accomodation/Comments/Comments.jsx';
@@ -29,19 +27,9 @@ export default function Accommodation() {
       {/* if currentAcco not found -> navigate to NotFound */}
       {currentAcco && (
         <>
-          <h2 className={styles.acco_title} >{currentAcco?.title}</h2>
-          <AccoSlider
-            titleImage={currentAcco?.titleImage}
-            images={currentAcco?.images}
-          />
           <AccoInfo currentAcco={currentAcco} />
-          {/* <AccoMap
-            lat={currentAcco?.latitude}
-            lon={currentAcco?.longitude}
-            title={currentAcco?.title}
-          /> */}
-          <Comments />
           <BookingPreview />
+          <Comments />
         </>
       )}
     </>
