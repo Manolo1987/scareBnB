2f52b79767bee19622b1d5617bb9aff9644aff67
manolo1987 <m.zeh.mz@gmail.com>
2025-01-23 15:48:06 +0100

    add children as prop to loading spinner

--- frontend/src/components/Shared/LoadingSpinner/LoadingSpinner.jsx
+++ frontend/src/components/Shared/LoadingSpinner/LoadingSpinner.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Spinner } from '@phosphor-icons/react';
 import styles from './LoadingSpinner.module.css';
 
-export default function LoadingSpinner() {
+export default function LoadingSpinner({children}) {
   return (
     <div className={styles.loadingContainer}>
       <Spinner size={32} className={styles.spinner} />
+      {children}
     </div>
   );
 }
