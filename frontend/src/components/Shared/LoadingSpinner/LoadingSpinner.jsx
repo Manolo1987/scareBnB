8c65975c7d6375f509c586f2895ec0ff821e6c96
manolo1987 <m.zeh.mz@gmail.com>
2025-01-23 15:44:06 +0100

    finish searchbar mobile style - add see all button

--- frontend/src/components/Shared/LoadingSpinner/LoadingSpinner.jsx
+++ frontend/src/components/Shared/LoadingSpinner/LoadingSpinner.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { Spinner } from '@phosphor-icons/react';
 import styles from './LoadingSpinner.module.css';
 
-export default function LoadingSpinner({ children }) {
+export default function LoadingSpinner() {
   return (
     <div className={styles.loadingContainer}>
       <Spinner size={32} className={styles.spinner} />
-      {children}
     </div>
   );
 }
