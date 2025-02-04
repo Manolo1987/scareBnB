1aa0b88303851f4da7a7a52fdd31b09e0bd9c945
Andreas <knopfandreas@arcor.de>
2025-02-04 08:37:12 +0100

    add className to main

--- frontend/src/pages/Layout/Layout.jsx
+++ frontend/src/pages/Layout/Layout.jsx
@@ -11,7 +11,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main>
+      <main className={styles.main}>
         <CookieBanner />
         <Outlet />
         <BackToTop />
